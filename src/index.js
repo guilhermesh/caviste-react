28f81bfec1d1249522724ce3fd88f536d947071b
guilhermesh <60899454+guilhermesh@users.noreply.github.com>
2021-11-19 19:38:22 +0100

    starting caviste react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,65 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class filterableProductTable extends React.Component {
+	render() {
+        return (
+			<div>
+				<div>
+					<div>
+						<input type='text' />
+					</div>
+					<div>
+						<input type='checkbox' />
+						<label>Show only red wines</label>
+					</div>
+				</div>
+				<div>
+					<div>
+						<table>
+							<thead>
+								<tr>
+									<th>Name</th>
+									<th>Price</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<th
+										colspan='2'
+										className='country'>
+										France
+									</th>
+								</tr>
+								<tr>
+									<td>Pinot Noir</td>
+									<td>€5,99</td>
+								</tr>
+								<tr>
+									<td>Pinot Noir</td>
+									<td>€5,99</td>
+								</tr>
+								<tr>
+									<th
+										colspan='2'
+										className='country'>
+										Italy
+									</th>
+								</tr>
+								<tr>
+									<td>Pinot Noir</td>
+									<td>€5,99</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	React.createElement(filterableProductTable, null),
+	document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
